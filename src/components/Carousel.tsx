9f1f26d990bb5f4c591a3d6522399dfe91e8f9ba
George <george.boujhenitsa@gmail.com>
2025-08-08 19:10:17 +0300

    add solution

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -19,44 +19,42 @@ const Carousel: React.FC<Props> = ({
   infinite,
 }: Props) => {
   const [items, setItems] = useState(images);
-  const [steps, setSteps] = useState(step);
-  const [pictureWidth, setPictureWidth] = useState(itemWidth);
-  const [sizeFrame, setSizeFrame] = useState(frameSize);
-  const [durationOfAnimation, setDurationOfAnimation] =
-    useState(animationDuration);
-  const [infiniteCarousel, setInfiniteCarousel] = useState(infinite);
+  const [nextDisable, setNextDisable] = useState(false);
+  const [prevDisable, setPrevDisable] = useState(false);
   const [transitionAnimation, setTransitionAnimation] = useState(true);
 
   const [, setMoveIndex] = useState(0);
   const [moveAmount, setMoveAmount] = useState(0);
 
   const getNext = () => {
-    const reminder = images.length % steps;
+    const reminder = images.length % step;
+
+    setPrevDisable(false);
 
     setMoveIndex(prevIndex => {
-      const nextIndex = prevIndex + steps;
+      const nextIndex = prevIndex + step;
 
-      if (infiniteCarousel) {
-        setMoveAmount(pictureWidth * steps);
+      if (infinite) {
+        setMoveAmount(itemWidth * step);
 
-        const lastPart = items.slice(0, steps);
+        const lastPart = items.slice(0, step);
 
         setTimeout(() => {
           setItems([
-            ...items.slice(steps, steps + steps),
-            ...items.slice(steps),
+            ...items.slice(step, step + step),
+            ...items.slice(step),
           ]);
-        }, durationOfAnimation);
+        }, animationDuration);
 
         setTimeout(() => {
           setTransitionAnimation(false);
           setMoveAmount(0);
-        }, durationOfAnimation * 1.2);
+        }, animationDuration * 1.2);
 
         setTimeout(() => {
-          setItems([...items.slice(steps), ...lastPart]);
+          setItems([...items.slice(step), ...lastPart]);
           setTransitionAnimation(true);
-        }, durationOfAnimation * 1.3);
+        }, animationDuration * 1.3);
       } else {
         if (prevIndex >= images.length - reminder) {
           return prevIndex;
@@ -64,10 +62,11 @@ const Carousel: React.FC<Props> = ({
 
         if (nextIndex === images.length - reminder) {
           const lastShown =
-            Math.max(steps, sizeFrame) - Math.min(steps, sizeFrame);
-          setMoveAmount(pictureWidth * (reminder + lastShown) + moveAmount);
+            Math.max(step, frameSize) - Math.min(step, frameSize);
+          setMoveAmount(itemWidth * (reminder + lastShown) + moveAmount);
+          setNextDisable(true);
         } else {
-          setMoveAmount(pictureWidth * nextIndex);
+          setMoveAmount(itemWidth * nextIndex);
         }
       }
 
@@ -76,17 +75,19 @@ const Carousel: React.FC<Props> = ({
   };
 
   const goPrev = () => {
-    const reminder = images.length % steps;
+    const reminder = images.length % step;
+
+    setNextDisable(false);
 
     setMoveIndex(prevIndex => {
-      const previousIndex = prevIndex - steps;
+      const previousIndex = prevIndex - step;
 
-      if (infiniteCarousel) {
+      if (infinite) {
         setTransitionAnimation(false);
-        setMoveAmount(pictureWidth * steps);
+        setMoveAmount(itemWidth * step);
         setItems([
-          ...items.slice(items.length - steps),
-          ...items.slice(0, items.length - steps),
+          ...items.slice(items.length - step),
+          ...items.slice(0, items.length - step),
         ]);
         setTimeout(() => {
           setTransitionAnimation(true);
@@ -99,10 +100,11 @@ const Carousel: React.FC<Props> = ({
 
         if (previousIndex === 0) {
           const lastShown =
-            Math.max(steps, sizeFrame) - Math.min(steps, sizeFrame);
-          setMoveAmount(pictureWidth * lastShown);
+            Math.max(step, frameSize) - Math.min(step, frameSize);
+          setMoveAmount(itemWidth * lastShown);
+          setPrevDisable(true);
         } else {
-          setMoveAmount(pictureWidth * (previousIndex - steps + reminder));
+          setMoveAmount(itemWidth * (previousIndex - step + reminder));
         }
       }
 
@@ -111,124 +113,51 @@ const Carousel: React.FC<Props> = ({
   };
 
   return (
-    <>
-      <label htmlFor="itemId">
-        Picture Width:
-        <input
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-            setPictureWidth(parseInt(e.target.value));
+    <div className="Carousel">
+      <div className="Carousel__list">
+        <button
+          onClick={() => {
+            goPrev();
           }}
-          id="itemId"
-          type="number"
-          name="pictureWidth"
-          value={pictureWidth}
-        />
-      </label>
-      <label htmlFor="stepId">
-        Steps:
-        <input
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-            setSteps(parseInt(e.target.value));
+          className={`prev ${prevDisable ? 'disabled' : ''}`}
+          type="button"
+        ></button>
+        <ul
+          className="Carousel__wrapper"
+          style={{
+            width: `${frameSize * itemWidth}px`,
+            overflow: 'hidden',
           }}
-          id="stepId"
-          type="number"
-          name="steps"
-          value={steps}
-        />
-      </label>
-      <label htmlFor="frameId">
-        FrameSize:
-        <input
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-            setSizeFrame(parseInt(e.target.value));
-          }}
-          id="frameId"
-          type="number"
-          name="frameSize"
-          value={sizeFrame}
-        />
-      </label>
-      <label htmlFor="durationId">
-        Animation Duration:
-        <input
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-            setDurationOfAnimation(parseInt(e.target.value));
-          }}
-          id="durationId"
-          type="number"
-          name="animationDuration"
-          value={durationOfAnimation}
-        />
-      </label>
-      <label htmlFor="infinite">
-        Infinite carousel:
-        <select
-          onChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
-            setInfiniteCarousel(e.target.value === 'true');
-          }}
-          name="infinite"
-          id="infinite"
-          value={infiniteCarousel ? 'true' : 'false'}
         >
-          <option value="true">True</option>
-          <option value="false">False</option>
-        </select>
-        {/* <input
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-            setDurationOfAnimation(parseInt(e.target.value));
+          {items.map((img, i) => {
+            return (
+              <li key={i}>
+                <img
+                  width={itemWidth}
+                  style={{
+                    transform: `translate(-${moveAmount}px)`,
+                    transition: transitionAnimation
+                      ? `transform ${animationDuration / 1000}s linear`
+                      : 'none',
+                  }}
+                  src={img}
+                  alt={`${items.indexOf(img) + 1}`}
+                  className="Carousel__img"
+                />
+              </li>
+            );
+          })}
+        </ul>
+        <button
+          data-cy="next"
+          onClick={() => {
+            getNext();
           }}
-          id="infinite"
-          type="number"
-          name="animationDuration"
-          value={durationOfAnimation}
-        /> */}
-      </label>
-      <div className="Carousel">
-        <div className="Carousel__list">
-          <button
-            onClick={() => {
-              goPrev();
-            }}
-            className="prev"
-            type="button"
-          ></button>
-          <ul
-            className="Carousel__wrapper"
-            style={{
-              width: `${sizeFrame * pictureWidth}px`,
-              overflow: 'hidden',
-            }}
-          >
-            {items.map((img, i) => {
-              return (
-                <li key={i}>
-                  <img
-                    width={pictureWidth}
-                    style={{
-                      transform: `translate(-${moveAmount}px)`,
-                      transition: transitionAnimation
-                        ? `transform ${durationOfAnimation / 1000}s linear`
-                        : 'none',
-                    }}
-                    src={img}
-                    alt={`${items.indexOf(img) + 1}`}
-                    className="Carousel__img"
-                  />
-                </li>
-              );
-            })}
-          </ul>
-          <button
-            data-cy="next"
-            onClick={() => {
-              getNext();
-            }}
-            className="next"
-            type="button"
-          ></button>
-        </div>
+          className={`next ${nextDisable ? 'disabled' : ''}`}
+          type="button"
+        ></button>
       </div>
-    </>
+    </div>
   );
 };
 
